122f7df06cabef8732b808ed31b9eb38e62b1efd
mabsagency <mabsagency1@gmail.com>
2026-08-27 16:20:28 +0200

    Fix Vercel asset base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/burgerlab-premium/',
+  base: process.env.VERCEL ? '/' : '/burgerlab-premium/',
   plugins: [react()],
   server: {
     port: 5173,
